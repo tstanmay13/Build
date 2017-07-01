6c868343716172195e5d71e20c40595c3648e7fb
Tanmay Singh <tstanmay13@gmail.com>
2017-07-01 11:48:34 -0500

    Initial commit

---
+++ app/src/main/java/tanmay/helloworld1/SecondActivity.java
@@ -0,0 +1,32 @@
+package tanmay.helloworld1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonClick();
+            }
+        });
+
+    }
+
+    private void buttonClick() {
+        TextView tv  = (TextView) findViewById(R.id.textView);
+        tv.setText("NewTextThatHasBeenChanged");
+}
+
+}
